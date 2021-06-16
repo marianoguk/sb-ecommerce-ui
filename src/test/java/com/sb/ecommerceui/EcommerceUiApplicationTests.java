4c3ea1a5da43a50326a1d4b1dfd984d2edef73db
marianoguk <mariano.garrote7@gmail.com>
2021-06-16 19:04:59 -0300

    Add files via upload
    Initial commit

---
+++ src/test/java/com/sb/ecommerceui/EcommerceUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.ecommerceui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
